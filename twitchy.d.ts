a07f9cd26d04028c31b5de0afbd113aa8d1894fa
josh <yurikoex@gmail.com>
2015-07-01 20:45:55 -0400

    init

--- twitchy.d.ts
+++ twitchy.d.ts
@@ -1,24 +1,21 @@
-module twitchy {
-  interface typedScope {
-      streams: IStream[];
-      alerts:any;
-  }
 
-  interface IStreamRequest {
-      limit: number;
-      offset?: number;
-  }
-  
-  interface IStream {
-      id: number;
-      game: string;
-      viewers: number;
-      preview: string;
-      displayName: string;
-      logo: string;
-      statusMessage: string;
-      url: string;
-      followers: number;
-      views: number;
-  }
+declare module "twitchy" {
+
+    export interface IStreamRequest {
+        limit: number;
+        offset?: number;
+    }
+
+    export interface IStream {
+        id: number;
+        game: string;
+        viewers: number;
+        preview: string;
+        displayName: string;
+        logo: string;
+        statusMessage: string;
+        url: string;
+        followers: number;
+        views: number;
+    }
 }
